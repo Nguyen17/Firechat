d53eb154b303720e4fa3b175fefa254f32cfbc87
Joshua Ramos <Krevalz@Joshuas-MacBook-Pro.local>
2017-09-10 17:30:22 -0700

    Add channel table

---
+++ Firechat/Model/Channel.swift
@@ -0,0 +1,9 @@
+//
+//  Channel.swift
+//  Firechat
+//
+//  Created by Joshua Ramos on 9/10/17.
+//  Copyright © 2017 Krevalent. All rights reserved.
+//
+
+import Foundation
---
+++ Firechat/New Group/ChannelTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChannelTableViewCell.swift
+//  Firechat
+//
+//  Created by Joshua Ramos on 9/10/17.
+//  Copyright © 2017 Krevalent. All rights reserved.
+//
+
+import UIKit
+
+class ChannelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
